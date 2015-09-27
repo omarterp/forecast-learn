fe195d29663c9362e0e240f58cf5668f895ec19e
Eric Perales <omarterp@gmail.com>
2015-09-27 09:48:49 -0400

    cap_one_forecasting init

---
+++ cap_one_forecasting.R
@@ -0,0 +1,17 @@
+# Forecasting Capital One stock Price
+require("Quandl")
+require("dplyr")
+#library(Quandl)#install.packages("dplyr")
+
+# Quandl API Key
+Quandl.auth("o7yyDXksn3Y9B-szcmfp")
+
+#Quandl.search("Capital One")
+#Quandl.search("COF")
+
+
+cof_price_history <- Quandl("YAHOO/COF")
+
+head(cof_price_history)
+cof_price_history$WeekNumber <- as.numeric(format(cof_price_history$Date, "%U"))
+head(cof_price_history)
