8d9a0b0b26431c673122559ad81fb08a69abd19c
Eric Perales <omarterp@gmail.com>
2015-09-28 09:27:31 -0400

    added earnings report

--- cap_one_forecasting.R
+++ cap_one_forecasting.R
@@ -8,6 +8,7 @@ Quandl.auth("o7yyDXksn3Y9B-szcmfp")
 
 #Quandl.search("Capital One")
 #Quandl.search("COF")
+#Quandl.search("COF")
 
 
 cof_price_history <- Quandl("YAHOO/COF")
@@ -16,3 +17,12 @@ head(cof_price_history)
 cof_price_history$WeekNumber <- as.numeric(format(cof_price_history$Date, "%U"))
 head(cof_price_history)
 
+#head(Quandl("GOOG/NYSE_COF"))
+
+
+cof_earnings <- Quandl("ZES/COF")
+
+cof_earnings_report <- Quandl("ZEE/COF_Q")
+
+
+str(cof_earnings)
